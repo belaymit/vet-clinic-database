97915c48b73ac6cfde29b4211e2c2356b0f0d4f4
belaymit <2belamit@gmail.com>
2023-02-21 10:20:49 +0300

    added owner and species tables and modified animals table

--- schema.sql
+++ schema.sql
@@ -10,3 +10,24 @@ CREATE TABLE animals (
 	weight_kg DECIMAL(10, 2) NOT NULL,
 	species VARCHAR(100)
 );
+
+-- Added Owners table 
+
+CREATE TABLE owners (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+        full_name VARCHAR(100) NOT NULL,
+        age INT NOT NULL
+);
+
+-- Added Species table
+
+CREATE TABLE species (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL
+);
+
+-- Alterd animals table and added foreign key
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id BIGINT REFERENCES species (id);
+ALTER TABLE animals ADD owner_id BIGINT REFERENCES owners (id);
