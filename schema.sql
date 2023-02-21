d4503652513915a839fb84eb49e1b997a3a54dcb
belaymit <2belamit@gmail.com>
2023-02-21 08:59:05 +0300

    updated table by adding 'species' column

--- schema.sql
+++ schema.sql
@@ -1,14 +1,12 @@
 /* Database schema to keep the structure of entire database. */
+DROP TABLE animals;
 
 CREATE TABLE animals (
-    name varchar(100)
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	escape_attempt INT,
+	neutered BOOLEAN NOT NULL,
+	weight_kg DECIMAL(10, 2) NOT NULL,
+	species VARCHAR(100)
 );
-
-CREATE TABLE animals (
-        id BIGSERIAL NOT NULL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        date_of_birth DATE NOT NULL,
-        escape_attempts INTEGER NOT NULL,
-        neutered BOOL NOT NULL,
-        weight_kg DECIMAL(4,2) NOT NULL
-        );
