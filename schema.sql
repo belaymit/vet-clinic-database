749489935695a04d00a47071ca80c5f0626d7b10
belaymit <2belamit@gmail.com>
2023-02-23 08:45:27 +0300

    added join tables - visits, vets and specializations

--- schema.sql
+++ schema.sql
@@ -31,3 +31,30 @@ CREATE TABLE species (
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD species_id BIGINT REFERENCES species (id);
 ALTER TABLE animals ADD owner_id BIGINT REFERENCES owners (id);
+
+-- Create a table named vets: 
+
+CREATE TABLE vets (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	age INT NOT NULL,
+	date_of_graduation DATE
+);
+
+-- There is a many-to-many relationship between the tables species and vets: a vet can specialize in multiple species, and a species can have multiple vets specialized in it. Create a "join table" called specializations to handle this relationship.
+
+CREATE TABLE specializations (
+	species_id BIGINT REFERENCES species (id),
+	vets_id BIGINT REFERENCES vets (id)
+);
+
+-- There is a many-to-many relationship between the tables animals and vets: 
+-- an animal can visit multiple vets and one vet can be visited by multiple animals.
+--  Create a "join table" called visits to handle this relationship, 
+-- it should also keep track of the date of the visit.
+
+CREATE TABLE visits (
+	animals_id BIGINT REFERENCES animals (id),
+	vets_id BIGINT REFERENCES vets (id),
+	date_of_visit DATE
+);
